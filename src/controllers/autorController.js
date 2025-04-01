0735d035a98e43979efd7b73801a69968d6687b5
jose amilton tavares freire junior <amilton.freire@icc.org.br>
2025-04-01 11:29:07 -0300

    refactor: adding verification in routes with id

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -2,63 +2,85 @@ import mongoose from "mongoose";
 import { autor } from "../models/Autor.js";
 
 class AutorController {
-  static listarAutores = async (req, res) => {
-    try {
-      const listaAutor = await autor.find({});
-      res.status(200).json(listaAutor);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: `${error.message}- Falha na requisição!` });
-    }
-  };
-  static listaAutorById = async (req, res) => {
-    try {
-      const id = req.params.id;
-      const listaById = await autor.findById(id);
+    static listarAutores = async (req, res) => {
+        try {
+            const listaAutor = await autor.find({});
+            res.status(200).json(listaAutor);
+        } catch (error) {
+            res.status(500).json({
+                message: `${error.message}- Falha na requisição!`,
+            });
+        }
+    };
+    static listaAutorById = async (req, res) => {
+        try {
+            const id = req.params.id;
+            const listaById = await autor.findById(id);
 
-      if (!listaById) {
-        res.status(404).send({ message: "Id do autor não encontrado" });
-        return;
-      }
-      res.status(200).json(listaById);
-    } catch (error) {
-      if (error instanceof mongoose.Error.CastError) {
-        res
-          .status(400)
-          .send({ message: "Os Dados fornecidos estão incorretos" });
-      }
-      res.status(500).json({ message: "Falha na requisição!" });
-    }
-  };
-  static cadastrarAutor = async (req, res) => {
-    try {
-      await autor.create(req.body);
-      res.status(200).json("Autor criado com sucesso!");
-    } catch (error) {
-      res.status(500).json("Falha ao criar usuário!");
-    }
-  };
-  static atualizarAutor = async (req, res) => {
-    try {
-      const id = req.params.id;
-      await autor.findByIdAndUpdate(id, req.body);
-      res.status(200).json("Autor atualizado com sucesso!");
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: `${error.message}- Falha na requisição!` });
-    }
-  };
-  static deletarAutor = async (req, res) => {
-    try {
-      const id = req.params.id;
-      await autor.findByIdAndDelete(id);
+            if (!listaById) {
+                res.status(404).send({ message: "Id do autor não encontrado" });
+                return;
+            }
+            res.status(200).json(listaById);
+        } catch (error) {
+            if (error instanceof mongoose.Error.CastError) {
+                res.status(400).send({
+                    message: "Os Dados fornecidos estão incorretos",
+                });
+            }
+            res.status(500).json({ message: "Falha na requisição!" });
+        }
+    };
+    static cadastrarAutor = async (req, res) => {
+        try {
+            await autor.create(req.body);
+            res.status(200).json("Autor criado com sucesso!");
+        } catch (error) {
+            res.status(500).json("Falha ao criar usuário!");
+        }
+    };
+    static atualizarAutor = async (req, res) => {
+        try {
+            const id = req.params.id;
+            const atualizaAutor = await autor.findByIdAndUpdate(id, req.body);
+            if (!atualizaAutor) {
+                res.status(404).send({
+                    message: "Id do autor não encontrado!",
+                });
+                return;
+            }
+            res.status(200).json("Autor atualizado com sucesso!");
+        } catch (error) {
+            if (error instanceof mongoose.Error.CastError) {
+                res.status(400).send({
+                    message: "Dados fornecidos estão incorretos",
+                });
+            }
+            res.status(500).json({
+                message: `${error.message}- Falha na requisição!`,
+            });
+        }
+    };
+    static deletarAutor = async (req, res) => {
+        try {
+            const id = req.params.id;
+            const deletarAutor = await autor.findByIdAndDelete(id);
 
-      res.status(200).json("Autor deletado com sucesso!");
-    } catch (error) {
-      res.status(500).json({ message: `${error.message}- Falha ao deletar!` });
-    }
-  };
+            if (!deletarAutor) {
+                res.status(404).send({ message: "Id do autor não encontrado" });
+                return;
+            }
+            res.status(200).json("Autor deletado com sucesso!");
+        } catch (error) {
+            if (error instanceof mongoose.Error.CastError) {
+                res.status(400).send({
+                    message: "Dados fornecidos estão incorretos",
+                });
+            }
+            res.status(500).json({
+                message: `${error.message}- Falha ao deletar!`,
+            });
+        }
+    };
 }
 export default AutorController;
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,68 +1,113 @@
+import mongoose from "mongoose";
 import livro from "../models/Livro.js";
 
 class LivroController {
-  static async listarLivros(req, res) {
-    try {
-      const listaLivros = await livro.find({});
-      res.status(200).json(listaLivros);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: `${error.message}- falha na requisição` });
+    static async listarLivros(req, res) {
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } catch (error) {
+            res.status(500).json({
+                message: `${error.message}- falha na requisição`,
+            });
+        }
     }
-  }
 
-  static async listarLivroPorID(req, res) {
-    try {
-      const id = req.params.id;
-      const livroEncontrado = await livro.findById(id);
-      res.status(200).json(livroEncontrado);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: `${error.message}- falha na requisição` });
+    static async listarLivroPorID(req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+
+            if (!livroEncontrado) {
+                res.status(400).send({
+                    message: "Id do livro não encontrado!",
+                });
+                return;
+            }
+
+            res.status(200).json(livroEncontrado);
+        } catch (error) {
+            if (error instanceof mongoose.Error.CastError) {
+                res.status(404).send({
+                    message: "Dados fornecidos estão incorretos",
+                });
+            }
+            res.status(500).json({
+                message: `${error.message}- falha na requisição`,
+            });
+        }
     }
-  }
-  static async cadastrarLivro(req, res) {
-    try {
-      const novoLivro = await livro.create(req.body);
-      res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: `${error.message}-falha ao cadastrar livro` });
+    static async cadastrarLivro(req, res) {
+        try {
+            const novoLivro = await livro.create(req.body);
+            res.status(201).json({
+                message: "criado com sucesso",
+                livro: novoLivro,
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: `${error.message}-falha ao cadastrar livro`,
+            });
+        }
     }
-  }
-  static async atualizarLivro(req, res) {
-    try {
-      const id = req.params.id;
-      await livro.findByIdAndUpdate(id, req.body);
-      res.status(200).json({ message: "livro atualizado" });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: `${error.message}- falha na atualização` });
+    static async atualizarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            const atualizaLivro = await livro.findByIdAndUpdate(id, req.body);
+
+            if (!atualizaLivro) {
+                res.status(400).send({
+                    message: "Id do livro não encontrado!",
+                });
+                return;
+            }
+
+            res.status(200).json({ message: "livro atualizado" });
+        } catch (error) {
+            if (error instanceof mongoose.Error.CastError) {
+                res.status(404).send({
+                    message: "Dados fornecidos estão incorretos",
+                });
+            }
+
+            res.status(500).json({
+                message: `${error.message}- falha na atualização`,
+            });
+        }
     }
-  }
-  static async deletarLivro(req, res) {
-    try {
-      const id = req.params.id;
-      await livro.findByIdAndDelete(id);
-      res.status(200).json({ message: "livro excluído" });
-    } catch (error) {
-      res.status(500).json({ message: $`falha na exclusão` });
+    static async deletarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            const deletaLivro = await livro.findByIdAndDelete(id);
+
+            if (!deletaLivro) {
+                res.status(400).send({
+                    message: "Id do livro não encontrado!",
+                });
+                return;
+            }
+
+            res.status(200).json({ message: "livro excluído" });
+        } catch (error) {
+            if (error instanceof mongoose.Error.CastError) {
+                res.status(404).send({
+                    message: "Dados fornecidos estão incorretos!",
+                });
+            }
+
+            res.status(500).json({ message: $`falha na exclusão` });
+        }
     }
-  }
-  static async listarLivrosEditora(req, res) {
-    const editora = req.query.editora;
-    try {
-      const livroPorEditora = await livro.find({ editora: editora });
-      res.status(200).json(livroPorEditora);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: `${error.message}- falha na requisição` });
+    static async listarLivrosEditora(req, res) {
+        const editora = req.query.editora;
+        try {
+            const livroPorEditora = await livro.find({ editora: editora });
+            res.status(200).json(livroPorEditora);
+        } catch (error) {
+            res.status(500).json({
+                message: `${error.message}- falha na requisição`,
+            });
+        }
     }
-  }
 }
 export default LivroController;
