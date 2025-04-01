cbebb35055089ac3b01af956b0a26f96d2ab242b
Junior Tavares <contatoojuniortavares@gmail.com>
2025-03-31 21:45:58 -0300

    refactor:adjustiment in autor-controller

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -1,7 +1,8 @@
+import mongoose from "mongoose";
 import { autor } from "../models/Autor.js";
 
 class AutorController {
-  static async listarAutores(req, res) {
+  static listarAutores = async (req, res) => {
     try {
       const listaAutor = await autor.find({});
       res.status(200).json(listaAutor);
@@ -10,27 +11,35 @@ class AutorController {
         .status(500)
         .json({ message: `${error.message}- Falha na requisição!` });
     }
-  }
-  static async listaAutorById(req, res) {
+  };
+  static listaAutorById = async (req, res) => {
     try {
       const id = req.params.id;
       const listaById = await autor.findById(id);
+
+      if (!listaById) {
+        res.status(404).send({ message: "Id do autor não encontrado" });
+        return;
+      }
       res.status(200).json(listaById);
     } catch (error) {
-      res
-        .status(500)
-        .json({ message: `${error.message}- Falha na requisição!` });
+      if (error instanceof mongoose.Error.CastError) {
+        res
+          .status(400)
+          .send({ message: "Os Dados fornecidos estão incorretos" });
+      }
+      res.status(500).json({ message: "Falha na requisição!" });
     }
-  }
-  static async cadastrarAutor(req, res) {
+  };
+  static cadastrarAutor = async (req, res) => {
     try {
       await autor.create(req.body);
       res.status(200).json("Autor criado com sucesso!");
     } catch (error) {
       res.status(500).json("Falha ao criar usuário!");
     }
-  }
-  static async atualizarAutor(req, res) {
+  };
+  static atualizarAutor = async (req, res) => {
     try {
       const id = req.params.id;
       await autor.findByIdAndUpdate(id, req.body);
@@ -40,8 +49,8 @@ class AutorController {
         .status(500)
         .json({ message: `${error.message}- Falha na requisição!` });
     }
-  }
-  static async deletarAutor(req, res) {
+  };
+  static deletarAutor = async (req, res) => {
     try {
       const id = req.params.id;
       await autor.findByIdAndDelete(id);
@@ -50,6 +59,6 @@ class AutorController {
     } catch (error) {
       res.status(500).json({ message: `${error.message}- Falha ao deletar!` });
     }
-  }
+  };
 }
 export default AutorController;
